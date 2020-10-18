43742c70f048ae73bad991304ac965f2f3e9c4a7
Thiago Allisson <allissonribeiro02@gmail.com>
2020-10-18 00:33:16 -0300

    Corrigindo erro na recuperação de dados dos orfanatos

--- src/views/orphanages_view.ts
+++ src/views/orphanages_view.ts
@@ -5,6 +5,7 @@ export default {
   render(orphanage: Orphanage) {
     return {
       id: orphanage.id,
+      name: orphanage.name,
       latitude: orphanage.latitude,
       longitude: orphanage.longitude,
       about: orphanage.about,
